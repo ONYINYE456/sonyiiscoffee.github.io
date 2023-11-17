285f12fa14c22d5f445fcd50d0b62d8bbad519ea
udeagbala onyinye sonia <soniaonyii@yahoo.com>
2023-11-17 12:54:17 +0100

    Add files via upload
    add

---
+++ index.js
@@ -0,0 +1,15 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#bars-btn').onclick = () => {
+  navbar.classList.toggle('active');
+};
+
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.toggle('active');
+};
+
+let cartItems = document.querySelector('.cart-items-container');
+document.querySelector('#cart-btn').onclick = () => {
+  navbar.classList.toggle('active');
+};
+
